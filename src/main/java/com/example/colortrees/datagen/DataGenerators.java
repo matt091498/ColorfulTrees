546b708cdd0be9fa35bf806b72af133cd1b78d0d
matt091498 <MattG0914@gmail.com>
2023-01-23 13:47:09 -0800

    Fixed issues caused by 1.18 to 1.19 port

--- src/main/java/com/example/colortrees/datagen/DataGenerators.java
+++ src/main/java/com/example/colortrees/datagen/DataGenerators.java
@@ -5,7 +5,7 @@ import com.example.colortrees.colortrees;
 import net.minecraft.data.DataGenerator;
 import net.minecraftforge.eventbus.api.SubscribeEvent;
 import net.minecraftforge.fml.common.Mod;
-import net.minecraftforge.forge.event.lifecycle.GatherDataEvent;
+import net.minecraftforge.data.event.GatherDataEvent;
 
 @Mod.EventBusSubscriber(modid = colortrees.MODID, bus = Mod.EventBusSubscriber.Bus.MOD)
 public class DataGenerators {
@@ -14,18 +14,13 @@ public class DataGenerators {
     public static void gatherData(GatherDataEvent event) {
         DataGenerator generator = event.getGenerator();
 
-        if (event.includeServer()) {
-            generator.addProvider(new CTRecipes(generator));
-            generator.addProvider(new CTLootTables(generator));
-            CTBlockTags blockTags = new CTBlockTags(generator, event.getExistingFileHelper());
-            generator.addProvider(blockTags);
-            generator.addProvider(new CTItemTags(generator, blockTags, event.getExistingFileHelper()));
-        }
-
-        if (event.includeClient()) {
-            generator.addProvider(new CTBlockStates(generator, event.getExistingFileHelper()));
-            generator.addProvider(new CTItemModels(generator, event.getExistingFileHelper()));
-            generator.addProvider(new CTLanguageProvider(generator, "en_us"));
-        }
+        generator.addProvider(event.includeServer(), new CTRecipes(generator));
+        generator.addProvider(event.includeServer(), new CTLootTables(generator));
+        CTBlockTags blockTags = new CTBlockTags(generator, event.getExistingFileHelper());
+        generator.addProvider(event.includeServer(), blockTags);
+        generator.addProvider(event.includeServer(), new CTItemTags(generator, blockTags, event.getExistingFileHelper()));
+        generator.addProvider(event.includeClient(), new CTBlockStates(generator, event.getExistingFileHelper()));
+        generator.addProvider(event.includeClient(), new CTItemModels(generator, event.getExistingFileHelper()));
+        generator.addProvider(event.includeClient(), new CTLanguageProvider(generator, "en_us"));
     }
 }
