45c6db2ccf652b09787828e057630adab94ac39a
matt091498 <MattG0914@gmail.com>
2023-01-24 15:20:38 -0800

    ported from 1.19.2 to 1.19.3

--- src/main/java/com/example/colortrees/datagen/CTItemTags.java
+++ src/main/java/com/example/colortrees/datagen/CTItemTags.java
@@ -2,15 +2,23 @@ package com.example.colortrees.datagen;
 
 import com.example.colortrees.colortrees;
 
-import net.minecraft.data.DataGenerator;
-import net.minecraft.data.tags.BlockTagsProvider;
+import net.minecraft.data.PackOutput;
+import net.minecraft.core.HolderLookup;
+import net.minecraftforge.common.data.BlockTagsProvider;
 import net.minecraft.data.tags.ItemTagsProvider;
 import net.minecraftforge.common.data.ExistingFileHelper;
 
+import java.util.concurrent.CompletableFuture;
+
 public class CTItemTags extends ItemTagsProvider {
 
-    public CTItemTags(DataGenerator generator, BlockTagsProvider blockTags, ExistingFileHelper helper) {
-        super(generator, blockTags, colortrees.MODID, helper);
+    public CTItemTags(PackOutput packOutput, CompletableFuture<HolderLookup.Provider> lookupProvider, BlockTagsProvider blockTags, ExistingFileHelper helper) {
+        super(packOutput, lookupProvider, blockTags, colortrees.MODID, helper);
+    }
+
+    @Override
+    protected void addTags(HolderLookup.Provider pProvider) {
+
     }
 
     @Override
